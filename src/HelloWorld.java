819c55c838dc11301efd8d7055ea9921adf5af53
Tetiana <mail4me4work@gmail.com>
2022-10-20 22:10:59 +0300

    After lecture 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,10 +15,12 @@ public class HelloWorld {
         twoDimArray[0][1] = 7;
         twoDimArray[0][2] = 3;
         twoDimArray[0][3] = 17;
+
         twoDimArray[1][0] = 7;
         twoDimArray[1][1] = 0;
         twoDimArray[1][2] = 1;
         twoDimArray[1][3] = 12;
+
         twoDimArray[2][0] = 8;
         twoDimArray[2][1] = 1;
         twoDimArray[2][2] = 2;
@@ -27,6 +29,8 @@ public class HelloWorld {
         int[][] twoDimArrayAlter = {{5, 7, 3, 17}, {7, 0, 1, 12}, {8, 1, 2, 3}};
 
         int[][] twoDimArrayPrint = {{5, 7, 3, 17}, {7, 0, 1, 12}, {8, 1, 2, 3}};//объявили массив и заполнили его элементами
+        int[][] twoDimArrayPrintSame = {{5, 7, 3, 17}, {7, 0, 1, 12}, {8, 1, 2, 3}, {46, 5, 77, 1}};//объявили массив и заполнили его элементами
+
         for (int i = 0; i < 3; i++) {  //идём по строкам
             for (int j = 0; j < 4; j++) {//идём по столбцам
                 System.out.print(" " + twoDimArrayPrint[i][j] + " "); //вывод элемента
@@ -38,6 +42,7 @@ public class HelloWorld {
         System.out.println(Arrays.deepToString(myArray));
 
         int[][] twoDimArrayLength = {{5, 7, 3, 17}, {7, 0, 1, 12}, {8, 1, 2, 3}};
+
         System.out.println(twoDimArray.length); //3
 
         System.out.println(twoDimArray[0].length); //4
@@ -50,7 +55,8 @@ public class HelloWorld {
         //создаём второй двумерный массив трёхмерного массива размерностью 1х1
         threeDimArr[1] = new int[1][1];
 
-        //задаем булев трёхмерный массив. На этой парковке есть 3 этажа, на каждом из которых можно поместить 2х5 = 10 машин. По умолчанию все ячейки пусты (false)
+        //задаем булев трёхмерный массив. На этой парковке есть 3 этажа,
+        // на каждом из которых можно поместить 2х5 = 10 машин. По умолчанию все ячейки пусты (false)
         boolean[][][] parkingLot = new boolean[3][2][5];
         //приехало две машины и припарковались на нулевом этаже в ячейке [1][0] и [1][3]
         parkingLot[0][1][0] = true;
@@ -58,12 +64,14 @@ public class HelloWorld {
 
         //Выведем массив в консоль
         for (int i = 0; i < 3; i++) {
+            //-------------------------------
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 5; k++) {
                     System.out.print("arr[" + i + "][" + j + "][" + k + "] = " + parkingLot[i][j][k] + "\t");
                 }
                 System.out.println();
             }
+            //---------------------------------------------------
         }
 
         //--------------------------------------------------
@@ -78,22 +86,27 @@ public class HelloWorld {
 
         //-----------------------------------------
         //Fibonacci
-        int n = 25;
+        //с помощью рекурсии (метод recursive) и цикла for (метод calculateWithFor):
+        //Если запустить данную программу и посмотреть на время выполнения обоих алгоритмов, то можно увидеть, что рекурсивный вариант работает дольше.
+        //Если изменить значение переменной n на 250, программа зависнет. Проблема состоит в том, что скорость вычисления рекурсивного алгоритма крайне низкая.
+        int n = 50;
         LocalTime localTime1 = LocalTime.now();
         System.out.print("Рекурсия " + recursive(n));
+
         LocalTime localTime2 = LocalTime.now();
-        Duration duration1 = Duration.between(localTime1, localTime2);
+        long duration1 = Duration.between(localTime1, localTime2).getSeconds();
         System.out.println(", время выполнения - " + duration1);
 
         LocalTime localTime3 = LocalTime.now();
         System.out.print("Цикл for " + calculateWithFor(n));
+
         LocalTime localTime4 = LocalTime.now();
-        Duration duration2 = Duration.between(localTime3, localTime4);
+        long duration2 = Duration.between(localTime3, localTime4).getSeconds();
         System.out.println(", время выполнения - " + duration2);
 
         //-------------------------------------
         //swap
-//1
+        //1 Вводим временную переменную, которая на время придержит значение из одной переменной:
         int a = 3;
         int b = 5;
 
@@ -103,16 +116,17 @@ public class HelloWorld {
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-//2
-        int a = 3;
-        int b = 5;
 
-        a = a + b; // a = 8, b = 5
-        b = a - b; // a = 8, b = 3
-        a = a - b; // a = 5, b = 3
+        //2Третья переменная не вводится, обмен достигается путем сложения и вычитания:
+        int c = 3;
+        int d = 5;
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        c = c + d; // c = 8, d = 5
+        d = c - d; // c = 8, d = 3
+        c = c - d; // c = 5, d = 3
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
         //--------------------------------
         //reverse
@@ -155,6 +169,31 @@ public class HelloWorld {
 
     }
 
+    //Сложность О(n) Фибоначчи
+    static long calculateWithFor(int n) {
+
+        long first = 0;
+        long second = 1;
+        long result = n;
+        for (int i = 1; i < n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+
+    }
+
+
+    //Сложность О(2^n) Фибоначчи
+    static long recursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return recursive(n - 2) + recursive(n - 1);
+    }
+
+    //selection
     private static void sortSelection(int[] array) {
         //Внешний цикл for отвечает за номер прохода,
         // а внутренний - за поиск минимального значения в текущем проходе.
@@ -175,6 +214,7 @@ public class HelloWorld {
         }
     }
 
+    //bubble
     private static void sort(int[] array) {
         // i - номер прохода
         for (int i = 0; i < array.length - 1; i++) {
@@ -189,6 +229,7 @@ public class HelloWorld {
         }
     }
 
+    //reverse array
     private static void invert(int[] array) {
         for (int i = 0; i < array.length / 2; i++) {
             int tmp = array[i];
@@ -197,37 +238,8 @@ public class HelloWorld {
         }
     }
 
-    /**
-     * Сложность О(n) Фибоначчи
-     *
-     * @param n
-     * @return
-     */
-    static long calculateWithFor(int n) {
-        long first = 0;
-        long second = 1;
-        long result = n;
-        for (int i = 1; i < n; i++) {
-            result = first + second;
-            first = second;
-            second = result;
-        }
-        return result;
-    }
-
-    /**
-     * Сложность О(2^n) Фибоначчи
-     *
-     * @param n
-     * @return
-     */
-    static long recursive(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return recursive(n - 2) + recursive(n - 1);
-    }
-
+    //1. Линейный поиск
+    //Самый простой, но и самый долгий алгоритм. Перебираем элементы массива и сравниваем с elementToSearch, который мы должны найти.
     public static int linearSearch(int[] array, int elementToSearch) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == elementToSearch) {
@@ -237,6 +249,14 @@ public class HelloWorld {
         return -1;
     }
 
+    //2. Двоичный поиск, итеративный подход
+    //Для использования алгоритма, массив должен быть отсортирован.
+    // Идея метода состоит в том, что мы делим массив пополам, берем "средний элемент" с индексом middleIndex,
+    // и сравниваем с искомым.
+    // Если они равны, мы заканчиваем поиск.
+    // Если искомый элемент меньше "среднего элемента" мы отбрасываем правую часть массива, иначе - левую.
+    // После чего повторяем эти операции снова и снова, пока искомый элемент не будет найден, или пока новый отрезок не станет пустым.
+    // Если элемент не нашелся возвращаем значение -1.
     public static int binarySearch(int[] array, int elementToSearch) {
         int firstIndex = 0;
         int lastIndex = array.length - 1;
@@ -263,6 +283,7 @@ public class HelloWorld {
         return -1;
     }
 
+    //3. Двоичный поиск, рекурсивный подход
     public static int recursiveBinarySearch(int[] array, int firstElement, int lastElement, int elementToSearch) {
         // условие прекращения
         if (lastElement >= firstElement) {
@@ -285,6 +306,7 @@ public class HelloWorld {
         return -1;
     }
 
+    //4. Поиск прыжками
     public static int jumpSearch(int[] array, int elementToSearch) {
         int arrayLength = array.length;
         int jumpStep = (int) Math.sqrt(array.length);
