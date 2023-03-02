6f69a2658479f0428fae4335092a5d7f9121b9c8
CuteKirichenko <mail4me4work@gmail.com>
2023-03-02 21:04:15 +0200

    Add Operators

---
+++ src/main/java/operators/Operators.java
@@ -0,0 +1,46 @@
+package operators;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("---------BigDecimal----------");
+        double d = 2.0 - 1.1;
+        System.out.println(d);
+
+        BigDecimal numberFromString = new BigDecimal("2.0");
+        System.out.println(numberFromString);
+
+        BigDecimal number1 = BigDecimal.valueOf(2.0);
+        BigDecimal number2 = BigDecimal.valueOf(1.1);
+        System.out.println(number1.subtract(number2));
+
+        System.out.println("----------ROUNDED-----------");
+        BigDecimal num = new BigDecimal("3.145");
+        BigDecimal roundedHalfUp = num.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal roundedHalfDown = num.setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal roundedUp = num.setScale(2, RoundingMode.UP);
+        BigDecimal roundedDown = num.setScale(2, RoundingMode.DOWN);
+        System.out.println(roundedHalfUp);
+        System.out.println(roundedHalfDown);
+        System.out.println(roundedUp);
+        System.out.println(roundedDown);
+
+        System.out.println("---------------COMPARE--------------");
+        BigDecimal num1 = new BigDecimal("5.6");
+        BigDecimal num2 = new BigDecimal("5.6");
+
+        int resultComparing = num1.compareTo(num2);
+        System.out.println(resultComparing);
+
+        System.out.println("---------------OTHER TYPES--------------");
+        double doubleValue = num.doubleValue(); // d = 3.145
+        int intValue = num.intValue(); // i = 3
+        long longValue = num.longValue(); // l = 3
+
+        System.out.println(doubleValue);
+        System.out.println(intValue);
+        System.out.println(longValue);
+    }
+}
